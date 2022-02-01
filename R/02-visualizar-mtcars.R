fbc77c6db3f4a2d21dce8031cbe05d5b4ad3167a
Daniela Goretti Castillo Leon <danigore22@gmail.com>
2022-02-01 12:47:30 -0600

    Primeros archivos de R

---
+++ R/02-visualizar-mtcars.R
@@ -0,0 +1,12 @@
+## Configura tu usuario de GitHub
+usethis::edit_git_config()
+
+[user]
+  name = Daniela Goretti Castillo Leon
+  email = danigore22@gmail.com
+
+## Para inicializar el repositorio de Git
+usethis::use_git()
+
+## Para conectar tu repositorio local de Git con los servidores de GitHub
+usethis::use_github()
