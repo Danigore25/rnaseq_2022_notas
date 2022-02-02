81f07bc3645159cb6510ea09a29e8fe15f978114
Daniela Goretti Castillo Leon <danigore22@gmail.com>
2022-02-01 20:35:17 -0600

    Actualizacion de archivo de analisis de paquetes de Bioconductor

---
+++ R/03-paquetes.R
@@ -0,0 +1,14 @@
+## ACTIVIDAD GRUPAL: SELECCION DE PAQUETES
+
+## Paquete: microbiomeMarker
+## Resumen: Facilita la identificacion de marcadores microbianos.
+## Plataformas: todas
+## Respuesta pronta a dudas: en ocasiones
+## Enlace: 	https://bioconductor.org/packages/microbiomeMarker/
+
+## Paquete: MicrobiomeProfiler
+## Resumen: Ayuda al analisis de datos bacterianos.
+## Plataformas: todas
+## Respuesta pronta a dudas: no
+## Enlace: https://bioconductor.org/packages/MicrobiomeProfiler/
+
