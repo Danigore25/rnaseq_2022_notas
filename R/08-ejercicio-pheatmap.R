05f2bdfae05b01b110371be4520da85ffd9a6ed7
Daniela Goretti Castillo Leon <danigore22@gmail.com>
2022-02-03 23:07:13 -0600

    Se creo el archivo del ejercicio referente a pheatmap

---
+++ R/08-ejercicio-pheatmap.R
@@ -0,0 +1,16 @@
+## EJERCICIO
+## Agregar nombres a pheatmap
+
+# Propuesta: con la paqueteria pheatmap se realiza este grafico, modificando la
+# opcion de rownames (el nombre de los genes) de FALSE a TRUE
+
+## Funcion match
+
+# Ayuda a buscar valores en un vector dentro de otro, de la forma:
+# match(x, table)
+# Donde x es el vector donde se busca la informacion y table es lo que se quiere
+# buscar.
+
+# Se puede buscar en rowdata o rowranges los IDs que queremos analizar, y por
+# medio de la funcion es posible encontrar el nombre de los genes dentro de
+# rowData.
